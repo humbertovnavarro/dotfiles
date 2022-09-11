b1fd02b706e85f581b602c82d35f74615720327b
humbertovnavarro <humberto.navarro.dev@gmail.com>
2022-09-10 22:50:38 -0700

    e

--- install.sh
+++ install.sh
@@ -1,32 +1,30 @@
 #!/bin/sh
-sudo pacman -S \
-sway \
-waybar \
-base-devel \
-git \
-cantarell-fonts \
-ttf-font-awesome \
-doas \
-zsh \
-rofi \
-neovim \
-mako \
-git-lfs \
+
+echo "bootstrapping aur"
+sudo pacman -S git base-devel
+cd /tmp
+git clone https://aur.archlinux.org/yay.git
+cd /tmp/yay
+makepkg si
+
+echo "installing packages"
+yay -S - < packages.txt
+
+echo "setting up doas"
+echo "permit :wheel" | sudo tee /etc/doas.conf
+
+
+echo "setting up dotfile repo"
 cd ~
 git init
 git remote add origin https://github.com/humbertovnavarro/dotfiles.git
 git pull origin main
-echo "permit :wheel" | sudo tee /etc/doas.conf
 
-
-# rustup
-
+echo "setting up rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# nvm 
-
+echo "setting up nvm/nodejs"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-# ohmyzsh
-
+echo "setting up ohmyzsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
