415ea89e942859432ed55d740de4981b8d3bdb0e
humbertovnavaro <humberto.navarro.dev@gmail.com>
2022-09-15 19:02:40 -0700

    updated installer script

--- .rice/install.sh
+++ .rice/install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# My custom install, blocks marked as optional can be omitted.
+user=kamaii
+
 
 echo "bootstrapping aur"
 sudo pacman -S git base-devel
@@ -16,16 +19,22 @@ git pull origin main
 
 cd ~/.rice
 
+sudo systemctl enable bluetooth
+sudo usermod -aG docker $user
+
+#optional if you know what you're doing
 echo "setting up automatic login, I hope you're using encryption :)"
 sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
 cat autologin.conf | sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf
 
+#optional, sudo is included for backwards compatability, you can remove sudo if you want post install
 echo "setting up doas"
 echo "permit :wheel" | sudo tee /etc/doas.conf
 
 echo "installing packages"
 yay -S - < packages.txt
 
+# everything beyond this point is optional 
 echo "setting up rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
@@ -38,4 +47,3 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 echo "setting up astro nvim"
 git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 
-sudo systemctl enable bluetooth
